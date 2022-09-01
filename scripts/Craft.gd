2025d9b529d219ae6995f286e22d22c41d7a3c5a
Maroxis <wola1997@interia.pl>
2022-09-01 19:11:23 +0200

    Graphics update

--- scripts/Craft.gd
+++ scripts/Craft.gd
@@ -2,7 +2,7 @@ extends "res://scripts/BaseActivity.gd"
 
 onready var tab_container: TabContainer = $TabContainer
 onready var tabAmm = tab_container.get_child_count()
-onready var tab_label: Label = $Category/BG/Control/Label
+#onready var tab_label: Label = $Category/BG/Control/Label
 onready var TabSwitcherContainer: VBoxContainer = $TabSwitcher/VBoxContainer
 onready var selected: TextureRect = $TabSwitcher/Control/Selected
 
@@ -30,15 +30,15 @@ func refresh():
 			for item in items:
 				if(item.name != "Margin" and item.name != "Margin2"):
 					item.refresh()
-	tab_label.text = tab_container.get_children()[tab_container.current_tab].name
+#	tab_label.text = tab_container.get_children()[tab_container.current_tab].name
 
 
 func _on_Prev_pressed() -> void:
 	if(tab_container.current_tab > 0):
 		tab_container.current_tab -= 1
-		tab_label.text = tab_container.get_children()[tab_container.current_tab].name
+#		tab_label.text = tab_container.get_children()[tab_container.current_tab].name
 
 func _on_Next_pressed() -> void:
 	if(tab_container.current_tab < tabAmm-1):
 		tab_container.current_tab += 1
-		tab_label.text = tab_container.get_children()[tab_container.current_tab].name
+#		tab_label.text = tab_container.get_children()[tab_container.current_tab].name
