1a13df151799f5a428067a600b0a97adb1f9abfa
Maroxis <wola1997@interia.pl>
2022-10-06 23:46:49 +0200

    res weight indicator

--- scripts/MissionSelect.gd
+++ scripts/MissionSelect.gd
@@ -1,10 +1,12 @@
 extends Control
 
 onready var resName: Label = $VBox/Name
-onready var ammount: Label = $VBox/Ammount
+onready var ammount: Label = $"%Ammount"
 onready var time: Label = $VBox/Time
 onready var button: Button = $VBox/Button/Button
 onready var buttonContainer: Control = $VBox/Button
+onready var weight: Label = $"%Weight"
+onready var weight_container: TextureRect = $VBox/HBoxContainer/WeightContainer
 
 onready var tool_req: TextureRect = $VBox/Button/ToolReq
 onready var tool_req_tier: Label = $VBox/Button/ToolReq/Tier
@@ -14,6 +16,7 @@ onready var sound: AudioStreamPlayer = $Sound
 
 export var connectMission = true
 export var foodRes = false
+export(String, "resources", "food", "meds", "null") var type = "resources"
 signal missionSelected
 
 func updateGatherTime(tm):
@@ -35,6 +38,10 @@ func populate(rsNam,amm,tm,tlReq):
 	if(typeof(amm) != TYPE_STRING):
 		ammount.text +="x"
 	time.text = Global.timeGetFullFormat(tm)
+	if type != "null":
+		weight.text = str(Inventory[type][rsNam]["weight"])
+	else:
+		weight_container.hide()
 	icon.texture =  load("res://sprites/Icons/64x64px/"+rsNam.to_lower()+".png")
 	if(tlReq):
 		tool_req.texture = load("res://sprites/Icons/32x32px/"+tlReq["tool"].to_lower()+".png")
