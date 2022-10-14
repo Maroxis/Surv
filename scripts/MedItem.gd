f3f28a78f29375529183922ec51aeee035edeef8
Maroxis <wola1997@interia.pl>
2022-10-14 06:47:02 +0200

    Meds Apply visual overhaul

--- scripts/MedItem.gd
+++ scripts/MedItem.gd
@@ -1,18 +1,31 @@
 extends IndicatorItem
 
-onready var buffs: VBoxContainer = $VBoxContainer/Buffs
+onready var sick_shielding_rate: Control = $VBoxContainer/Buffs/SickShieldingRate
+onready var sick_reduction_rate: Control = $VBoxContainer/Buffs/SickReductionRate
+onready var health_regen_rate: Control = $VBoxContainer/Buffs/HealthRegenRate
+onready var time: TextureRect = $Time
+
+func _ready() -> void:
+	sick_shielding_rate.visible = false
+	sick_reduction_rate.visible = false
+	health_regen_rate.visible = false
 
 func addBuffs(list):
 	if list.has("sickGain"):
-		addLabel("Sick gain rate",list["sickGain"])
+		sick_shielding_rate.visible = true
+		setLabel(sick_shielding_rate,1/list["sickGain"])
 	if list.has("sickReduction"):
-		addLabel("Sick reduction rate",list["sickReduction"])
+		sick_reduction_rate.visible = true
+		setLabel(sick_reduction_rate,list["sickReduction"])
 	if list.has("healthRegen"):
-		addLabel("Health regen rate",list["healthRegen"])
+		health_regen_rate.visible = true
+		setLabel(health_regen_rate,list["healthRegen"])
 	if list.has("time"):
-		addLabel("Buff time",Global.timeGetFullFormat(list["time"],false,true))
+		time.visible = true
+		setLabel(time,Global.timeGetFullFormat(list["time"],false,true),false)
 
-func addLabel(desc,val):
-	var new_label = Label.new()
-	buffs.add_child(new_label)
-	new_label.text = str(desc) + ": " + str(val)
+func setLabel(node,val, x = true):
+	label = node.get_node("Label")
+	label.text = str(val)
+	if x:
+		label.text += "x"
