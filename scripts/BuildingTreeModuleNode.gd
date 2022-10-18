0c3c5ac8f15c0c6464a0cfb7d25bb17ef3f6e09a
Maroxis <wola1997@interia.pl>
2022-10-18 05:33:11 +0200

    buildingTreeViewer scroll,module select

--- scripts/BuildingTreeModuleNode.gd
+++ scripts/BuildingTreeModuleNode.gd
@@ -5,7 +5,11 @@ var level
 var column
 var orgOffset = Vector2(0,0)
 var initialized = false
+var yOffset = 0
 onready var texture_rect: TextureRect = $TextureRect
+onready var tool_button: Button = $CanvasLayer/ToolButton
+
+signal selectedNode
 
 func init(nm,lv,col = null):
 	moduleName = nm
@@ -19,9 +23,31 @@ func init(nm,lv,col = null):
 		texture_rect.texture = load("res://sprites/Icons/64x64px/"+nm.to_lower()+".png")
 	else:
 		texture_rect.texture = load("res://sprites/Icons/256x256px/"+nm.to_lower()+".png")
-	
+
+func set_bought():
+	self.overlay = GraphNode.OVERLAY_POSITION
+func set_avaliable():
+	self.overlay = GraphNode.OVERLAY_BREAKPOINT
+	tool_button.visible = true
+
+func update_pos_x(x):
+	self.offset.x = x
+	tool_button.rect_position.x = x
+
+func update_pos_y(y):
+	self.offset.y = y
+	tool_button.rect_position.y = y + yOffset
+
+func update_button_offset(off):
+	yOffset = -off
+	update_pos_y(self.offset.y)
 
 func _on_Module_offset_changed() -> void:
-	if initialized:
-		if self.offset != orgOffset:
-			self.offset = orgOffset
+	tool_button.rect_position.x = self.offset.x
+	tool_button.rect_position.y = self.offset.y + yOffset
+#	if initialized:
+#		if self.offset != orgOffset:
+#			self.offset = orgOffset
+
+func _on_Button_pressed() -> void:
+	emit_signal("selectedNode",self)
--- scripts/BuildingTreeViewer.gd
+++ scripts/BuildingTreeViewer.gd
@@ -3,17 +3,20 @@ extends SceneLoader
 var building
 onready var graph_edit: GraphEdit = $GraphEdit
 onready var scene = load("res://nodes/components/BuildingTreeModuleNode.tscn")
+onready var v_scroll_bar: VScrollBar = $VScrollBar
+
+signal scrollChanged
+
 func _ready() -> void:
 	graph_edit.get_zoom_hbox().visible = false
-	var offset = 0
+	var offset = 16
 	for structure in Buildings.Structure:
 		offset = create_modules(structure,offset)
-	
+	v_scroll_bar.max_value = offset - 720 + 32
 
 func create_modules(structure,mainOffset):
-	print(mainOffset)
-	var curOffset = mainOffset
 	var nodes = {}
+	var curOffset = mainOffset
 	building = Buildings.Structure[structure]
 	var modnum = 0
 	for module in Buildings.Structure[structure]:
@@ -34,11 +37,11 @@ func create_modules(structure,mainOffset):
 			nodes[module].push_back(mod)
 			mod.offset.x = (tr)*(mod.rect_size.x + 32)
 			mod.offset.y = (modnum)*(mod.rect_size.y + 4) + mainOffset
+			mod.connect("selectedNode",self,"_on_GraphEdit_node_selected")
+			connect("scrollChanged",mod,"update_button_offset")
 			mod.init(module,tier,tr)
 			if tr == 1:
-#				mod.set_slot_enabled_left(0, false)
-# warning-ignore:return_value_discarded
-				pass
+				mod.set_avaliable()
 			if not moduleDict.has("tier"+str(tier+1)) and not moduleDict["benefitsText"].has("enable"):
 				mod.set_slot_enabled_right(0, false)
 			if tier > 1:
@@ -61,17 +64,20 @@ func create_modules(structure,mainOffset):
 	nodes["structure"].set_slot_enabled_left(0, false)
 	nodes["structure"].offset.y = (mainOffset + curOffset) / 2 - nodes["structure"].rect_size.y
 	nodes["structure"].init(structure,null)
+	nodes["structure"].set_bought()
 	for mod in nodes:
-		print(mod)
 		if typeof(nodes[mod]) == TYPE_ARRAY and nodes[mod][0].column == 1:
+# warning-ignore:return_value_discarded
 			graph_edit.connect_node(nodes["structure"].name,0,nodes[mod][0].name,0)
 	return curOffset
 
-
 func _on_GraphEdit_node_selected(node: Node) -> void:
-	print(node)
+	print(node.moduleName)
+
+func _on_Return_Button_pressed() -> void:
+	hide()
 
 
-func _on_GraphEdit_scroll_offset_changed(ofs: Vector2) -> void:
-	graph_edit.scroll_offset.x = 0
-	print(ofs)
+func _on_VScrollBar_value_changed(value: float) -> void:
+	graph_edit.scroll_offset.y = value
+	emit_signal("scrollChanged",value)
