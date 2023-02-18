b2411c6dfd61970a3faa0923bbc2c283f55a58a7
Maroxis <wola1997@interia.pl>
2023-02-19 00:24:54 +0100

    event visual improvement

--- scripts/EventPopup.gd
+++ scripts/EventPopup.gd
@@ -9,18 +9,21 @@ func _ready() -> void:
 	Global.EventPopup = self
 
 func show():
-	self.rect_position.y = self.rect_size.y
+#	self.rect_position.y = self.rect_size.y
+	self.rect_scale = Vector2(0,0)
 	visible = true
-	var tween = create_tween().set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "rect_position:y", 0.0, 0.5)
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+#	tween.tween_property(self, "rect_position:y", 0.0, 0.5)
+	tween.tween_property(self, "rect_scale", Vector2(1.0,1.0), 1.2)
 	tween.tween_property(texture_button, "disabled", false, 0.1)
 	Global.Sound.play(Sound.UI_EVENTS, "SFX")
 	
 
 func hide():
 	texture_button.disabled = true
-	var tween = create_tween().set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "rect_position:y", self.rect_size.y, 0.7)
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+#	tween.tween_property(self, "rect_position:y", self.rect_size.y, 0.7)
+	tween.tween_property(self, "rect_scale", Vector2(0.0,0.0), 0.3)
 	tween.tween_property(self, "visible", false, 0.1)
 
 func populate(tit,dsc,res):
