03edd6ff9760370e4603270a3ccb6b3dac956b0e
Maroxis <wola1997@interia.pl>
2023-02-19 20:39:19 +0100

    event popup visual and animations

--- scripts/EventPopup.gd
+++ scripts/EventPopup.gd
@@ -3,28 +3,37 @@ onready var texture_button: TextureButton = $"%TextureButton"
 onready var title: Label = $"%Title"
 onready var desc: Label = $"%Desc"
 onready var result: Label = $"%Result"
+onready var scroll_border: NinePatchRect = $"%ScrollBorder"
+onready var mask_light: Light2D = $"%MaskLight"
 
 
 func _ready() -> void:
 	Global.EventPopup = self
 
 func show():
-#	self.rect_position.y = self.rect_size.y
-	self.rect_scale = Vector2(0,0)
+	_deactivate_button()
+	scroll_border.rect_position.y = self.rect_size.y + scroll_border.rect_size.y
+	mask_light.position.y = self.rect_size.y * 1.5 + scroll_border.rect_size.y
 	visible = true
-	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
-#	tween.tween_property(self, "rect_position:y", 0.0, 0.5)
-	tween.tween_property(self, "rect_scale", Vector2(1.0,1.0), 1.2)
-	tween.tween_property(texture_button, "disabled", false, 0.1)
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel()
+	tween.connect("finished",self,"_activate_button")
+	tween.tween_property(scroll_border, "rect_position:y", -48.0, 0.9)
+	tween.tween_property(mask_light, "position:y", self.rect_size.y * 0.5, 0.9)
 	Global.Sound.play(Sound.UI_EVENTS, "SFX")
 	
+func _activate_button():
+	texture_button.disabled = false
+func _deactivate_button():
+	texture_button.disabled = true
+func _set_invisible():
+	self.visible = false
 
 func hide():
-	texture_button.disabled = true
-	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
-#	tween.tween_property(self, "rect_position:y", self.rect_size.y, 0.7)
-	tween.tween_property(self, "rect_scale", Vector2(0.0,0.0), 0.3)
-	tween.tween_property(self, "visible", false, 0.1)
+	_deactivate_button()
+	var tween = create_tween().set_ease(Tween.EASE_IN).set_parallel()
+	tween.connect("finished",self,"_set_invisible")
+	tween.tween_property(scroll_border, "rect_position:y", self.rect_size.y + scroll_border.rect_size.y, 0.4)
+	tween.tween_property(mask_light, "position:y", self.rect_size.y * 1.5 + scroll_border.rect_size.y, 0.4)
 
 func populate(tit,dsc,res):
 	title.text = tit
