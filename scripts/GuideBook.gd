229fa6dd85b0b598a40cc626768c3f96fa298455
Maroxis <wola1997@interia.pl>
2023-02-25 00:17:40 +0100

    GuideBook player health

--- scripts/GuideBook.gd
+++ scripts/GuideBook.gd
@@ -1,7 +1,17 @@
 extends Control
 
+onready var tab_container: VBoxContainer = $Pages/TabContainer
+onready var tab_content_container: TabContainer = $TabContentContainer
+
 func _ready() -> void:
 	Global.GuideBook = self
+	var tabs = tab_container.get_children()
+	for id in tabs.size():
+		tabs[id].tab = id
+		tabs[id].connect("tabClicked",self,"tabClicked")
 
 func toggle():
 	self.visible = !self.visible
+
+func tabClicked(tab):
+	tab_content_container.current_tab = tab
