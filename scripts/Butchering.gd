86256297f00b326742f7b5e01baaa60641eafeff
Maroxis <wola1997@interia.pl>
2023-07-02 08:39:05 +0200

    Revert "Cooking Butchering ui rework"
    This reverts commit 572861144fab954c088fb71d743d33fb40c545b7.

--- scripts/Butchering.gd
+++ scripts/Butchering.gd
@@ -1,9 +1,10 @@
 extends SceneLoader
 
-onready var carcass_select: Control = $HBoxContainer/VBoxContainer2/CarcassSelect
+onready var carcass_select: Control = $HBoxContainer/CarcassSelect
 onready var item_scene  = load("res://nodes/components/ItemSquareAmm.tscn")
 onready var output_container: VBoxContainer = $HBoxContainer/OutputContainer
-onready var time_label: Label = $"%TimeLabel"
+onready var butcher_button: ToolButton = $HBoxContainer/VBoxContainer/ButcherButton
+onready var time_label: Label = $HBoxContainer/VBoxContainer/TimeLabel
 
 var selectedCarcass
 
@@ -22,8 +23,7 @@ func addCarcassItems():
 	for res in Inventory.food:
 		if Inventory.food[res].has("carcass"):
 			carcass_select.add_item(res)
-			carcass_select.set_amm(Inventory.get_food_amm(res))
-	selectedCarcass = carcass_select.get_selected_item()
+	carcass_select.init()
 
 func butcher():
 	if Inventory.add_resource(selectedCarcass,-1,true):
@@ -33,7 +33,7 @@ func butcher():
 			var fd = Inventory.food.has(res)
 			Inventory.add_resource(res,amm,fd)
 		Player.pass_time(Inventory.food[selectedCarcass]["craftTime"])
-		carcass_select.shake_selected()
+		butcher_button.shakeSubtle()
 		refreshButton()
 		Achivements.animal_butchered()
 
@@ -53,18 +53,17 @@ func refreshTime():
 	time_label.text = getCraftTime()
 
 func refreshButton():
-	carcass_select.set_amm(Inventory.get_food_amm(selectedCarcass))
 	if(Inventory.get_food_amm(selectedCarcass) == 0):
-		carcass_select.toggle(false)
+		butcher_button.disable()
 	else:
-		carcass_select.toggle(true)
+		butcher_button.enable()
 
 func _on_CarcassSelect_itemSelected(item) -> void:
 	selectedCarcass = item
 	refresh()
 
+func _on_ButcherButton_pressed() -> void:
+	butcher()
+
 func _on_Butchering_visibility_changed() -> void:
 	refresh()
-
-func _on_CarcassSelect_itemClicked(_item) -> void:
-	butcher()
